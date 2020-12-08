30212ee8364583c6a6de00164e06114063442903
kasia.kordys <kasia.kordys@gmail.com>
2020-12-08 21:00:48 +0000

    add model level validation error

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -15,7 +15,37 @@ namespace ModelValidation.Controllers
         [HttpPost]
         public ActionResult MakeBooking(Appointment appo)
         {
-            return View("Completed", appo);
+
+            if(string.IsNullOrEmpty(appo.ClientName))
+            {
+                //ClientName - name of the property
+                ModelState.AddModelError("ClientName", "Please enter client name");
+            }
+
+            if(ModelState.IsValidField("Date") && DateTime.Now > appo.Date)
+            {
+                //can value be parsed?
+                ModelState.AddModelError("Date", "Please enter future date");
+            }
+
+            if(!appo.TermsAccepted)
+            {
+                ModelState.AddModelError("TermsAccepted", "Please accept terms & conditions");
+            }
+            //model level validation error
+            if (ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date") && appo.ClientName == "Gia" && appo.Date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                ModelState.AddModelError("", "Gia cannot book appointments on Saturdays");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appo);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
