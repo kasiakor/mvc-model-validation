1a3a648a1d95057c5d4fd57c98eb24eed401c368
kasia.kordys <kasia.kordys@gmail.com>
2020-12-08 19:37:53 +0000

    add controller actions and views

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ActionResult MakeBooking(Appointment appo)
+        {
+            return View("Completed", appo);
+        }
+    }
+}
