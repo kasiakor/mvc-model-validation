b0e30c297bcd7d4a3999bd2e74907b3981e35fd5
kasia.kordys <kasia.kordys@gmail.com>
2020-12-09 16:46:36 +0000

    add metadata to validation rules

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -5,9 +5,13 @@ namespace ModelValidation.Models
 {
     public class Appointment
     {
+        //validation is applied to all methods
+        [Required]
+        [Display(Name = "Client")]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Then date is obligatory")]
         public DateTime Date { get; set; }
         public bool TermsAccepted { get; set; }
 
